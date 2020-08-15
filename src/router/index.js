67e6c743aa9fa14537fc2fa86c1a8047ecf7d26d
luoyusoft <luoyusoft@126.com>
2020-08-15 15:54:37 +0800

    1、二级路径

--- src/router/index.js
+++ src/router/index.js
@@ -67,8 +67,7 @@ const mainRoutes = {
   }
 }
 const router = new Router({
-  base: '/manage/',
-  mode: 'history',
+  mode: 'hash',
   scrollBehavior: () => ({ y: 0 }), // 每次访问滚动条都置0
   isAddDynamicMenuRoutes: false, // 是否已经添加动态（菜单）路由
   routes: globalRoutes.concat(mainRoutes)
