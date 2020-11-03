d0065ceacb6991d40bc32bd709849771a2399e1c
luoyusoft <luoyusoft@126.com>
2020-11-03 23:54:39 +0800

    1. 修改后端端口

--- static/config/index-dev.js
+++ static/config/index-dev.js
@@ -4,5 +4,5 @@
 ;(function () {
   window.SITE_CONFIG = {}
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:9998/api/luoyublog/manage'
+  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8800/api/luoyublog/manage'
 })()
