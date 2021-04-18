54df3691e1022a448da3819453ce4c1b8e664055
jinhaoxun <jinhaoxun@zhuanzhuan.comssh-add>
2021-04-18 20:57:08 +0800

    1. 新增首页数据

--- src/views/common/components/panelGroup.vue
+++ src/views/common/components/panelGroup.vue
@@ -1,46 +1,54 @@
 <template>
   <el-row :gutter="40" class="panel-group">
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+      <div class="card-panel" @click="$router.push({ name: 'log-log' })">
+<!--        <div class="card-panel-icon-wrapper icon-people">-->
         <div class="card-panel-icon-wrapper icon-people">
           <icon-svg name="peoples" class-name="card-panel-icon" />
         </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">New Visits</div>
-          <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num"/>
+        <div class="card-panel-description" style="margin: 10px 0 0 10px" >
+<!--          <div class="card-panel-text">New Visits</div>-->
+<!--          <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num"/>-->
+          <span>今天PV：</span><count-to :start-val="0" :end-val="hommeInfo.logInfo.todayPV" :duration="3600" class="card-panel-num"/><br>
+          <span>今天UV：</span><count-to :start-val="0" :end-val="hommeInfo.logInfo.todayUV" :duration="3600" class="card-panel-num"/><br>
+          <span>总PV：</span><count-to :start-val="0" :end-val="hommeInfo.logInfo.allPV" :duration="3600" class="card-panel-num"/><br>
+          <span>总UV：</span><count-to :start-val="0" :end-val="hommeInfo.logInfo.allUV" :duration="3600" class="card-panel-num"/>
         </div>
       </div>
     </el-col>
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('messages')">
-        <div class="card-panel-icon-wrapper icon-message">
-          <icon-svg name="message" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">Messages</div>
-          <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num"/>
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('purchases')">
-        <div class="card-panel-icon-wrapper icon-money">
-          <icon-svg name="like" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">Likes</div>
-          <count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num"/>
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
+      <div class="card-panel" @click="$router.push({ name: 'article-article' })">
         <div class="card-panel-icon-wrapper icon-shopping">
           <icon-svg name="articles" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
-          <div class="card-panel-text">Articles</div>
-          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num"/>
+          <!--          <div class="card-panel-text">Articles</div>-->
+          <span>已发布：</span><count-to :start-val="0" :end-val="hommeInfo.articleInfo.publishCount" :duration="3600" class="card-panel-num"/><br>
+          <span>总量：</span><count-to :start-val="0" :end-val="hommeInfo.articleInfo.allCount" :duration="3600" class="card-panel-num"/>
+        </div>
+      </div>
+    </el-col>
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="$router.push({ name: 'video-video' })">
+        <div class="card-panel-icon-wrapper icon-message">
+          <icon-svg name="videos" class-name="card-panel-icon" />
+        </div>
+        <div class="card-panel-description">
+<!--          <div class="card-panel-text">Messages</div>-->
+          <span>已发布：</span><count-to :start-val="0" :end-val="hommeInfo.videoInfo.publishCount" :duration="3600" class="card-panel-num"/><br>
+          <span>总量：</span><count-to :start-val="0" :end-val="hommeInfo.videoInfo.allCount" :duration="3600" class="card-panel-num"/>
+        </div>
+      </div>
+    </el-col>
+    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+      <div class="card-panel" @click="$router.push({ name: 'operation-friendlink' })">
+        <div class="card-panel-icon-wrapper icon-money">
+          <icon-svg name="like" class-name="card-panel-icon" />
+        </div>
+        <div class="card-panel-description">
+<!--          <div class="card-panel-text">友链</div>-->
+          <span>友链量：</span><count-to :start-val="0" :end-val="hommeInfo.friendLinkInfo.count" :duration="3600" class="card-panel-num"/><br>
+          <span>推荐量：</span><count-to :start-val="0" :end-val="hommeInfo.recommendInfo.count" :duration="3600" class="card-panel-num"/>
         </div>
       </div>
     </el-col>
@@ -51,12 +59,94 @@
 import CountTo from 'vue-count-to'
 
 export default {
+  data () {
+    return {
+      hommeInfo: {
+        articleInfo: {
+          publishCount: 0,
+          allCount: 0
+        },
+        videoInfo: {
+          publishCount: 0,
+          allCount: 0
+        },
+        logInfo: {
+          todayPV: 0,
+          todayUV: 0,
+          allPV: 0,
+          allUV: 0
+        },
+        friendLinkInfo: {
+          count: 0
+        },
+        recommendInfo: {
+          count: 0
+        }
+      }
+    }
+  },
   components: {
     CountTo
+  },
+  activated () {
+    this.getHommeInfo()
   },
   methods: {
     handleSetLineChartData (type) {
       this.$emit('handleSetLineChartData', type)
+    },
+    // 获取首页数据
+    getHommeInfo () {
+      this.$http({
+        url: this.$http.adornUrl('/manage/article/homeinfo'),
+        method: 'get'
+      }).then((response) => {
+        if (response && response.code === 200) {
+          this.hommeInfo.articleInfo = response.data
+        } else {
+          this.$message.error(response.msg)
+        }
+      })
+      this.$http({
+        url: this.$http.adornUrl('/manage/video/homeinfo'),
+        method: 'get'
+      }).then((response) => {
+        if (response && response.code === 200) {
+          this.hommeInfo.videoInfo = response.data
+        } else {
+          this.$message.error(response.msg)
+        }
+      })
+      this.$http({
+        url: this.$http.adornUrl('/manage/log/homeinfo'),
+        method: 'get'
+      }).then((response) => {
+        if (response && response.code === 200) {
+          this.hommeInfo.logInfo = response.data
+        } else {
+          this.$message.error(response.msg)
+        }
+      })
+      this.$http({
+        url: this.$http.adornUrl('/manage/operation/friendlink/homeinfo'),
+        method: 'get'
+      }).then((response) => {
+        if (response && response.code === 200) {
+          this.hommeInfo.friendLinkInfo = response.data
+        } else {
+          this.$message.error(response.msg)
+        }
+      })
+      this.$http({
+        url: this.$http.adornUrl('/manage/operation/recommend/homeinfo'),
+        method: 'get'
+      }).then((response) => {
+        if (response && response.code === 200) {
+          this.hommeInfo.recommendInfo = response.data
+        } else {
+          this.$message.error(response.msg)
+        }
+      })
     }
   }
 }
@@ -109,7 +199,7 @@ export default {
     }
     .card-panel-icon-wrapper {
       float: left;
-      margin: 14px 0 0 14px;
+      margin: 14px 8px 0 10px;
       padding: 16px;
       transition: all 0.38s ease-out;
       border-radius: 6px;
@@ -119,10 +209,9 @@ export default {
       font-size: 48px;
     }
     .card-panel-description {
-      float: right;
+      //float: right;
       font-weight: bold;
-      margin: 26px;
-      margin-left: 0px;
+      margin: 26px 26px 26px 0px;
       .card-panel-text {
         line-height: 18px;
         color: rgba(0, 0, 0, 0.45);
@@ -130,7 +219,8 @@ export default {
         margin-bottom: 12px;
       }
       .card-panel-num {
-        font-size: 20px;
+        font-size: 15px;
+        color: red;
       }
     }
   }
